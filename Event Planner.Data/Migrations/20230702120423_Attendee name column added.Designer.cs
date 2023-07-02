a77f190d2164303adb6ba44d0e338f58b9500e64
David Nakov <nakov_david@mail.bg>
2023-07-02 15:05:18 +0300

    feat: added name column to the attendee table.

--- Event Planner.Data/Migrations/20230702120423_Attendee name column added.Designer.cs
+++ Event Planner.Data/Migrations/20230702120423_Attendee name column added.Designer.cs
@@ -4,6 +4,7 @@ using EventPlanner.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EventPlanner.Data.Migrations
 {
     [DbContext(typeof(EventPlannerDbContext))]
-    partial class EventPlannerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230702120423_Attendee name column added")]
+    partial class Attendeenamecolumnadded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Event Planner.Data/Models/Attendee.cs
+++ Event Planner.Data/Models/Attendee.cs
@@ -11,6 +11,9 @@
         public int Id { get; set; }
 
         [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
         public string Email { get; set; } = null!;
 
         [Required]
