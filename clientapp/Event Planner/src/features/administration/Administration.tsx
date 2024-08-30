e60fd8a3822678d879d3c30d6df0943e000dce2b
David Nakov <nakov_david@mail.bg>
2024-08-30 13:14:55 +0300

    refactor: cleanup and extracted the users query into a custom hook.

--- clientapp/Event Planner/src/features/administration/Administration.tsx
+++ clientapp/Event Planner/src/features/administration/Administration.tsx
@@ -1,29 +1,27 @@
 import { routePaths } from 'infrastructure/routing/routePaths';
 import { Link } from 'react-router-dom';
 
-export const Administration = () => {
-  return (
-    <>
-      <h1 className="text-2xl font-bold mb-12">Hi there, admin!</h1>
-      <p className="mb-5">Here you can manage some of the entities in the site!</p>
-      <Link
-        className="text-lg text-light-blue font-semibold"
-        to={routePaths.administrationCategories.path}
-      >
-        {routePaths.administrationCategories.name}
-      </Link>
-      <Link
-        className="text-lg text-light-blue font-semibold"
-        to={routePaths.administrationEvents.path}
-      >
-        {routePaths.administrationEvents.name}
-      </Link>
-      <Link
-        className="text-lg text-light-blue font-semibold"
-        to={routePaths.administrationUsers.path}
-      >
-        {routePaths.administrationUsers.name}
-      </Link>
-    </>
-  );
-};
+export const Administration = () => (
+  <>
+    <h1 className="text-2xl font-bold mb-12">Hi there, admin!</h1>
+    <p className="mb-5">Here you can manage some of the entities in the site!</p>
+    <Link
+      className="text-lg text-light-blue font-semibold"
+      to={routePaths.administrationCategories.path}
+    >
+      {routePaths.administrationCategories.name}
+    </Link>
+    <Link
+      className="text-lg text-light-blue font-semibold"
+      to={routePaths.administrationEvents.path}
+    >
+      {routePaths.administrationEvents.name}
+    </Link>
+    <Link
+      className="text-lg text-light-blue font-semibold"
+      to={routePaths.administrationUsers.path}
+    >
+      {routePaths.administrationUsers.name}
+    </Link>
+  </>
+);
