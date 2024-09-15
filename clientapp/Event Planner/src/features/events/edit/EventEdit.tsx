ab12b47beaf20cdef50c93ca20ad439bbc1025a5
David Nakov <nakovdavid98@gmail.com>
2024-09-15 22:29:54 +0300

    refactor: event edit page - use already existing hook to fetch the event;

--- clientapp/Event Planner/src/features/events/edit/EventEdit.tsx
+++ clientapp/Event Planner/src/features/events/edit/EventEdit.tsx
@@ -1,19 +1,13 @@
 import { CircularProgress } from '@mui/material';
 import { EventForm } from 'features/events/form/EventForm';
-import { IAllEventsEntity } from 'features/events/models/allEventsEntity';
-import { getRequestsOptions } from 'infrastructure/api/endpoints/getRequestsOptions';
-import { useReadQuery } from 'infrastructure/api/hooks/useReadQuery';
-import { replacePlaceholderWithId } from 'infrastructure/utilities/replacePlaceholderWithId';
 import { useParams } from 'react-router-dom';
+import { useEventDetailsQuery } from '../details/hooks/useEventDetailsQuery';
 
 export const EventEdit = () => {
   const { id } = useParams();
   if (!id) throw new Error('No id found!');
 
-  const { data: event, isLoading } = useReadQuery<IAllEventsEntity>({
-    endpoint: replacePlaceholderWithId(getRequestsOptions.GetSingleEvent.endpoint, id),
-    queryKey: [getRequestsOptions.GetSingleEvent.queryKey],
-  });
+  const { data: event, isLoading } = useEventDetailsQuery(id);
 
   return (
     <div className="my-10">
