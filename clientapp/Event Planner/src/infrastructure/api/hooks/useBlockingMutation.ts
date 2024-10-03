f508fc2073cf8aadc65ecee317312089aac6ae5f
David Nakov <nakovdavid98@gmail.com>
2024-10-03 19:51:13 +0300

    refactor: updated blocking mutation to use new blocker store;

--- clientapp/Event Planner/src/infrastructure/api/hooks/useBlockingMutation.ts
+++ clientapp/Event Planner/src/infrastructure/api/hooks/useBlockingMutation.ts
@@ -1,16 +1,17 @@
 import { UseMutationOptions } from '@tanstack/react-query';
-import { useAppContext } from 'AppContext';
+import { useBlockerStore } from 'AppContext';
 import { AxiosError } from 'axios';
 import { useAppMutation } from 'infrastructure/api/hooks/useAppMutation';
 import { IUseAppMutationOptions } from 'infrastructure/api/models/mutationOptions';
+import { useShallow } from 'zustand/react/shallow';
 
 export const useBlockingMutation = <TRequest, TResponse = void, TError = void>(
   options: IUseAppMutationOptions &
     UseMutationOptions<TResponse, AxiosError<TError>, TRequest>,
 ) => {
-  const {
-    blocker: { block, unblock },
-  } = useAppContext();
+  const { block, unblock } = useBlockerStore(
+    useShallow((s) => ({ block: s.block, unblock: s.unblock })),
+  );
 
   const onMutate = options.onMutate;
 
