b6884f131ad1d38cee8ddc3f2a63d9c52a03745e
David Nakov <nakovdavid98@gmail.com>
2024-10-21 20:02:30 +0300

    feat: added new entity - link;

---
+++ Event Planner.Data/Models/Link.cs
@@ -0,0 +1,22 @@
+﻿namespace EventPlanner.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Link
+    {
+        [Key]
+        public string Id { get; set; } = null!;
+
+        [Required]
+        [ForeignKey(nameof(Attendee))]
+        public int AttendeeId { get; set; }
+
+        [Required]
+        public Attendee Attendee { get; set; } = null!;
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime ValidTo { get; set; }
+    }
+}
