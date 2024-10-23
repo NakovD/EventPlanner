1640c7bdbc3bd6f80f0c13dbae0eac90e67136d7
David Nakov <nakovdavid98@gmail.com>
2024-10-23 18:55:25 +0300

    feat: added the link service contract;

---
+++ EventPlanner.Services/Contracts/ILinkService.cs
@@ -0,0 +1,11 @@
+﻿namespace EventPlanner.Services.Contracts
+{
+    using EventPlanner.Services.Models.Link;
+
+    public interface ILinkService
+    {
+        Task<( bool isSuccess, string? linkId)> CreateAsync(int attendeeId, DateTime validTo);
+
+        Task<LinkDto?> GetAsync(string linkId);
+    }
+}
