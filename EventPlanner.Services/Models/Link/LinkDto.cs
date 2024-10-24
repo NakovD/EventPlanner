07163db168051b1cb0090d1dbae586ec42d83750
David Nakov <nakovdavid98@gmail.com>
2024-10-24 20:11:18 +0300

    feat: added link dto and mapper profile;

---
+++ EventPlanner.Services/Models/Link/LinkDto.cs
@@ -0,0 +1,15 @@
+﻿namespace EventPlanner.Services.Models.Link
+{
+    using EventPlanner.Services.Models.Attendee;
+
+    public class LinkDto
+    {
+        public string Id { get; set; } = null!;
+
+        public AttendeeDto Attendee { get; set; } = null!;
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime ValidTo { get; set; }
+    }
+}
---
+++ EventPlanner.Services/Profiles/LinkProfile.cs
@@ -0,0 +1,14 @@
+﻿namespace EventPlanner.Services.Profiles
+{
+    using AutoMapper;
+    using EventPlanner.Data.Models;
+    using EventPlanner.Services.Models.Link;
+
+    public class LinkProfile : Profile
+    {
+        public LinkProfile()
+        {
+            CreateMap<Link, LinkDto>();
+        }
+    }
+}
