422e202cc34fdb4f0bf9cc5a3412956578f730ca
David Nakov <nakovdavid98@gmail.com>
2024-11-17 20:23:29 +0200

    refactor: event details;

--- clientapp/Event Planner/src/features/events/details/EventDetails.tsx
+++ clientapp/Event Planner/src/features/events/details/EventDetails.tsx
@@ -5,11 +5,22 @@ import { useEventDetails } from './hooks/useEventDetails';
 export const EventDetails = () => {
   const { canEdit, query } = useEventDetails();
 
-  const { isLoading, isError, data } = query;
+  const { isLoading, isError, data: event } = query;
 
   if (isLoading) return <p>Loading...</p>;
 
   if (isError) return <p>Error occured!</p>;
 
-  return <EventProfile event={data} canEdit={canEdit} />;
+  return (
+    <EventProfile>
+      <EventProfile.Image imageUrl={event.image} />
+      <EventProfile.Wrapper>
+        <EventProfile.Details event={event} />
+        {canEdit && <EventProfile.Edit eventId={event.id} />}
+        <EventProfile.Description description={event.description} />
+        <EventProfile.Attendees canEdit={canEdit} eventId={event.id} />
+        <EventProfile.Comments eventId={event.id} />
+      </EventProfile.Wrapper>
+    </EventProfile>
+  );
 };
