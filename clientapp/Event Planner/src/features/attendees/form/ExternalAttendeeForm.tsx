1f13e23c296c873e58d73eb12285fd1f03edc2f5
David Nakov <nakovdavid98@gmail.com>
2024-11-21 19:22:11 +0200

    refactor: external attendee form  to use the new textfield;

--- clientapp/Event Planner/src/features/attendees/form/ExternalAttendeeForm.tsx
+++ clientapp/Event Planner/src/features/attendees/form/ExternalAttendeeForm.tsx
@@ -1,6 +1,6 @@
 import { useExternalAttendeeForm } from 'features/attendees/form/hooks/useExternalAttendeeForm';
 import { Button } from 'features/common/button/Button';
-import { TextField } from 'features/common/form/TextField';
+import { Textfield } from 'features/common/form/Textfield';
 
 interface IExternalAttendeeFormProps {
   eventId: number;
@@ -10,8 +10,8 @@ export const ExternalAttendeeForm = ({ eventId }: IExternalAttendeeFormProps) =>
   const { control, handleSubmit } = useExternalAttendeeForm({ eventId });
   return (
     <form className="flex flex-col gap-3 w-2/4" onSubmit={handleSubmit}>
-      <TextField name="attendeeName" control={control} label="Attendee Name" />
-      <TextField name="attendeeEmail" control={control} label="Attendee Email" />
+      <Textfield name="attendeeName" control={control} label="Attendee Name" />
+      <Textfield name="attendeeEmail" control={control} label="Attendee Email" />
       <Button className="mt-3" type="submit" label="Submit" />
     </form>
   );
