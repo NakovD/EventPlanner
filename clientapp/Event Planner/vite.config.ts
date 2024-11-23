3125cc05995b55c3d8d00ec1a7889ef59446b74a
David Nakov <nakovdavid98@gmail.com>
2024-11-23 10:59:14 +0200

    feat: added vite ts checker;

--- clientapp/Event Planner/vite.config.ts
+++ clientapp/Event Planner/vite.config.ts
@@ -1,11 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 import mkcert from 'vite-plugin-mkcert';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), mkcert()],
+  plugins: [react(), tsconfigPaths(), mkcert(), checker({ typescript: true })],
   server: {
     port: 5100,
     https: true,
