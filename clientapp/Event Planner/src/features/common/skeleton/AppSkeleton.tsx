fdadb8b4ebbca862094766722133bb0ea2fc307f
David Nakov <nakovdavid98@gmail.com>
2024-12-09 20:15:18 +0200

    refactor: replaced mui skeleton with regular skeleton;

--- clientapp/Event Planner/src/features/common/skeleton/AppSkeleton.tsx
+++ clientapp/Event Planner/src/features/common/skeleton/AppSkeleton.tsx
@@ -1,17 +1,11 @@
-import { Skeleton } from '@mui/material';
-
-type SkeletonType = 'text' | 'rectangular' | 'rounded' | 'circular';
-
 interface IAppSkeletonProps {
   width: number | 'full-width';
   height: number;
-  type?: SkeletonType;
 }
 
-export const AppSkeleton = ({ width, height, type = 'rounded' }: IAppSkeletonProps) => (
-  <Skeleton
-    variant={type}
-    width={width === 'full-width' ? '100%' : width}
-    height={height}
+export const AppSkeleton = ({ width, height }: IAppSkeletonProps) => (
+  <div
+    className="animate-pulse bg-background-light opacity-75 rounded-3xl"
+    style={{ width: width === 'full-width' ? '100%' : width, height }}
   />
 );
