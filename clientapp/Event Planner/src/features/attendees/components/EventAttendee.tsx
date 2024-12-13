18f04b8ea1a715b4bd102b72e3c8bc5fecff02f1
David Nakov <nakovdavid98@gmail.com>
2024-12-13 20:09:16 +0200

    refactor: removed mui components from event attendee;

--- clientapp/Event Planner/src/features/attendees/components/EventAttendee.tsx
+++ clientapp/Event Planner/src/features/attendees/components/EventAttendee.tsx
@@ -1,4 +1,3 @@
-import { Card, CardContent } from '@mui/material';
 import { useDeleteAttendee } from 'features/attendees/hooks/useDeleteAttendee';
 import { IAttendee } from 'features/attendees/models/attendee';
 import { Button } from 'features/common/button/Button';
@@ -13,19 +12,17 @@ export const EventAttendee = ({ attendee }: IEventAttendeeProps) => {
     eventId: attendee.eventId,
   });
   return (
-    <Card className="border border-gray-200 rounded-lg shadow">
-      <CardContent>
-        <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
-          Name: {attendee.name}
-        </h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Email: {attendee.email}
-        </p>
-        <p className="text-sm text-center my-3">Status: {attendee.status}</p>
-        <p className="text-sm mt-3 text-right">
-          Action: <Button label="Remove" onClick={deleteAttendee} className="ml-3" />
-        </p>
-      </CardContent>
-    </Card>
+    <div className="border border-gray-200 rounded-lg shadow p-4">
+      <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+        Name: {attendee.name}
+      </h5>
+      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        Email: {attendee.email}
+      </p>
+      <p className="text-sm text-center my-3">Status: {attendee.status}</p>
+      <p className="text-sm mt-3 text-right">
+        Action: <Button label="Remove" onClick={deleteAttendee} className="ml-3" />
+      </p>
+    </div>
   );
 };
